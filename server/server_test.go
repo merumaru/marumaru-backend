3f88199e2e90afd81e1b1c51a24f4965411dc875
Prashant Anand <prs0704@gmail.com>
2019-11-08 18:05:50 +0900

    added basic test case and uncommented test line in config file

--- server/server_test.go
+++ server/server_test.go
@@ -1,17 +1,13 @@
 package server_test
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 
-	"github.com/merumaru/marumaru-backend/data"
-	"github.com/merumaru/marumaru-backend/models"
-
 	"github.com/gin-gonic/gin"
+	. "github.com/merumaru/marumaru-backend/server"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/merumaru/marumaru-backend/server"
 )
 
 func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
@@ -29,39 +25,20 @@ var _ = Describe("Server", func() {
 
 	BeforeEach(func() {
 		router = CreateRouter()
-		// Since we modify lists in memory, we need to restore them to a clean state before every test
-		data.Reload()
 	})
 
 	Describe("Version 1 API at /api/v1", func() {
 		Describe("The / endpoint", func() {
 			BeforeEach(func() {
-				response = performRequest(router, "GET", "/api/v1/")
+				response = performRequest(router, "GET", "/")
 			})
 
 			It("Returns with Status 200", func() {
 				Expect(response.Code).To(Equal(200))
 			})
 
-			It("Returns the String 'Hello World'", func() {
-				Expect(response.Body.String()).To(Equal("Hello World"))
-			})
-		})
-
-		Describe("GET the /pokemon endpoint", func() {
-			BeforeEach(func() {
-				response = performRequest(router, "GET", "/api/v1/pokemon")
-			})
-
-			It("Returns with Status 200", func() {
-				Expect(response.Code).To(Equal(200))
-			})
-
-			It("Returns all Pokemon", func() {
-				// To easily test JSON responses, you should unmarshal the response into an actual type, then compare to what you expect
-				var actual models.PokemonList
-				json.Unmarshal(response.Body.Bytes(), &actual)
-				Expect(actual).To(Equal(*data.Pokemon()))
+			It("Returns the String 'This is indeed a landing page'", func() {
+				Expect(response.Body.String()).To(Equal("This is indeed a landing page"))
 			})
 		})
 	})
