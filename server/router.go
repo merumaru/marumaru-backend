ec0767ab4c0398106f1bbf4104b455913bf46f3a
kaustubhhiware <hiwarekaustubh@gmail.com>
2019-11-11 19:29:04 +0900

    Fix urls

--- server/router.go
+++ server/router.go
@@ -13,21 +13,21 @@ func setupRoutes(router *gin.Engine) {
 	router.GET("/list", attachDB(client, listPage))
 
 	router.GET("/products", attachDB(client, getAllProductsHandler))
-	router.GET("/product/:id", attachDB(client, getProductByIDHandler))
-	router.GET("/product-user/:id", attachDB(client, getProductByUserIDHandler))
-	router.GET("/order-user/:id", attachDB(client, GetOrderByUserIDHandler))
-	router.GET("/order/:id", attachDB(client, getOrderByIDHandler))
-	router.POST("/product", attachDB(client, addProductHandler))
-	router.POST("/order", attachDB(client, addOrderHandler))
+	router.GET("/products/:id", attachDB(client, getProductByIDHandler))
+	router.GET("/products-user/:id", attachDB(client, getProductByUserIDHandler))
+	router.POST("/products", attachDB(client, addProductHandler))
+	router.POST("/products/:id/rent", attachDB(client, rentProductHandler))
+	router.PATCH("/products/:id/edit", attachDB(client, editProductHandler))
+
+	router.POST("/orders", attachDB(client, addOrderHandler))
+	router.GET("/orders-user/:id", attachDB(client, GetOrderByUserIDHandler))
+	router.GET("/orders/:id", attachDB(client, getOrderByIDHandler))
 
 	router.POST("/users/login", attachDB(client, Signin))
 	router.GET("/users/welcome", Welcome)
 	router.POST("/users/refresh", Refresh)
 	router.POST("/users/signup", attachDB(client, SignUp))
 	router.GET("/users", attachDB(client, GetUserByCookie))
-
-	router.POST("/products/:id/rent", attachDB(client, rentProductHandler))
-	router.PATCH("/edit/:id", attachDB(client, editProductHandler))
 }
 
 func attachDB(client *mongo.Client, fn func(*gin.Context, *mongo.Client)) gin.HandlerFunc {
